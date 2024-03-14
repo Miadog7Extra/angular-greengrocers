58beac53461ff6c1d9e2edd34acdee52dc6a15ed
Petter Karstensen <petterkarstensen@gmail.com>
2024-03-14 15:08:28 +0100

    Checkout working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckoutComponent } from './groceries/checkout/checkout.component';
 import { AppComponent } from './app.component';
+import { StoreComponent } from './groceries/store/store.component';
 
 const routes: Routes = [
-    {path: '', component: AppComponent},
+    {path: '', component: StoreComponent},
     {path: 'checkout', component: CheckoutComponent}
 ];
 
--- src/app/groceries/checkout/checkout.component.ts
+++ src/app/groceries/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GroceriesService } from '../services/groceries.service';
 
 @Component({
   selector: 'app-checkout',
@@ -12,6 +13,28 @@ export class CheckoutComponent {
 
   constructor() {
     console.log('CheckoutComponent initialized');
+  }
+  groceriesService = inject(GroceriesService)
+
+  cart = this.groceriesService.grocierieCart
+
+  total: number = 0;
+
+  ngOnInit() {
+    this.cart=this.groceriesService.grocierieCart
+  }
+
+  getTotal() {
+    this.total = 0;
+    for(let grocerie of this.cart.keys()){
+      //@ts-ignore
+      const itemPrice: number = grocerie.price*this.cart.get(grocerie)
+      this.total+=itemPrice
+    }
+    return this.total;
+  }
+  emptyCart() {
+    this.cart.clear()
   }
   
 }
